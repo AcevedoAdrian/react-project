e7ecc4fec2ef168201486daecf828f899c95e88e
AcevedoAdrian <acevedohugoadrian@gmail.com>
2024-12-20 18:06:31 -0300

    fix: Succes initial routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import AppRouter from "./routes/routes";
+import AppRouter from "./routes/routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
